03cbd9a7cdfeb487e8b8656581b3fdf637e658cc
Henryawd345 <henrygoodboy2000@gmail.com>
2025-10-26 02:00:21 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -7,7 +7,18 @@
 #include <algorithm>
 #include <iomanip>
 #include <chrono>
+
 using namespace std;
+using namespace std::chrono;
+
+vector<string> read_lines(string &path){
+    ifstream in(path);
+    vector<string> lines;
+    string s;
+    lines.reserve(20000);
+    while (getline(in, s)) lines.push_back(s);
+    return lines;
+}
 
 int main() {
     
