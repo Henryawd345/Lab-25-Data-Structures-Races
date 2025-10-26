b2d829de787377db1554adf135238b892d714610
Henryawd345 <henrygoodboy2000@gmail.com>
2025-10-26 02:17:17 -0700

    Sort section

--- main.cpp
+++ main.cpp
@@ -27,10 +27,10 @@ int main() {
       return 1;
     }
 
-    long long vec_read_us, lst_read_us, sor_read_us;
-    long long vec_sort_us, lst_sort_us, sor_sort_us = -1;
-    long long vec_ins_us,  lst_ins_us,  sor_ins_us;
-    long long vec_del_us,  lst_del_us,  sor_del_us;
+    long long vec_read_us, lst_read_us, set_read_us;
+    long long vec_sort_us, lst_sort_us, set_sort_us = -1;
+    long long vec_ins_us,  lst_ins_us,  set_ins_us;
+    long long vec_del_us,  lst_del_us,  set_del_us;
 
     //Read section
     {
@@ -51,17 +51,43 @@ int main() {
         auto start = high_resolution_clock::now();
         set<string> st(lines.begin(), lines.end());
         auto end = high_resolution_clock::now();
-        sor_read_us = duration_cast<microseconds>(end - start).count();
+        set_read_us = duration_cast<microseconds>(end - start).count();
     }
+
+    //sort section
+    { 
+        vector<string> v = lines;
+        auto start = high_resolution_clock::now();
+        sort(v.begin(), v.end());
+        auto end = high_resolution_clock::now();
+        vec_sort_us = duration_cast<microseconds>(end - start).count();
+    }
+    {
+        list<string> lst(lines.begin(), lines.end());
+        auto start = high_resolution_clock::now();
+        lst.sort();
+        auto end = high_resolution_clock::now();
+        lst_sort_us = duration_cast<microseconds>(end - start).count();
+    }
+    // set are already sorted
+
 
     cout << left << setw(12) << "Operation"
          << setw(12) << "Vector(us)"
          << setw(12) << "List(us)"
          << setw(12) << "Set(us)" << "\n";
+
     cout << left << setw(12) << "Read"
          << setw(12) << vec_read_us
          << setw(12) << lst_read_us
-         << setw(12) << sor_read_us << "\n";
+         << setw(12) << set_read_us << "\n";
+
+   cout << left << setw(12) << "Sort"
+         << setw(12) << vec_sort_us
+         << setw(12) << lst_sort_us
+         << setw(12) << set_sort_us << " (Already sorted)" <<"\n";
+
+    
 
     return 0;
 }
